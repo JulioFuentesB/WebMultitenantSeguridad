9a4caa77d06107d978c3629bd69addd751cf411c
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2021-07-13 16:35:31 -0500

    Agregar archivos de proyecto.

---
+++ WebMultitenantSeguridad/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Finbuckle.MultiTenant;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWeb.Models
+{
+
+
+    public class ApplicationUser : IdentityUser//MultiTenantIdentityUser
+    {
+
+        public string TenantId { get; set; }
+    }
+
+
+}
